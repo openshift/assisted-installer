f9596dfd86deb14e932d9e28c32f6dacd7524d21
Eran Cohen <eran@stratoscale.com>
2020-07-13 22:41:53 +0300

    Updated image to v4.5.1

--- src/utils/openshift_version.go
+++ src/utils/openshift_version.go
@@ -13,7 +13,7 @@ var getOpenshiftMapping = func(openshiftVersion string) map[string]string {
 			rhcos: "https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/4.4/latest/rhcos-4.4.3-x86_64-metal.x86_64.raw.gz",
 			mco:   "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:301586e92bbd07ead7c5d3f342899e5923d4ef2e0f1c0cf08ecaae96568d16ed"},
 		"4.5": {
-			rhcos: "https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/4.5/latest/rhcos-4.5.0-rc.7-x86_64-metal.x86_64.raw.gz", // latest image in the 4.5 mirror
+			rhcos: "https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/4.5/4.5.1/rhcos-4.5.1-x86_64-metal.x86_64.raw.gz",       // 4.5.1 image in the openshift mirror
 			mco:   "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:58bed0f5c4fbe453b994f6b606fefe3e2aaf1d10dcbdf5debb73b93007c7bee5"}, // MCO from latest 4.5 nightly https://mirror.openshift.com/pub/openshift-v4/clients/ocp-dev-preview/4.5.0-0.nightly-2020-05-14-021132/
 	}
 
