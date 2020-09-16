1b82dd467a448f7863b521a574c9718917dcac4b
Moti Asayag <masayag@redhat.com>
2020-09-16 12:49:38 +0300

    Add command env vars to error message
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- src/ops/ops.go
+++ src/ops/ops.go
@@ -109,15 +109,15 @@ func (o *ops) ExecCommand(liveLogger io.Writer, command string, args ...string) 
 
 		if exitErr, ok := err.(*exec.ExitError); ok {
 			if status, ok := exitErr.Sys().(syscall.WaitStatus); ok {
-				o.log.Debugf("failed executing %s %v, out: %s, with status %d",
-					command, args, output, status.ExitStatus())
+				o.log.Debugf("failed executing %s %v, env vars %v, out: %s, with status %d",
+					command, args, output, cmd.Env, status.ExitStatus())
 
-				return output, fmt.Errorf("failed executing %s %v , error %s. Output %s", command, args, exitErr, output)
+				return output, fmt.Errorf("failed executing %s %v, env vars %v, error %s. Output %s", command, args, cmd.Env, exitErr, output)
 			}
 		}
-		return output, fmt.Errorf("failed executing %s %v , error %s. Output %s", command, args, err, output)
+		return output, fmt.Errorf("failed executing %s %v, env vars %v, error %s. Output %s", command, args, cmd.Env, err, output)
 	}
-	o.log.Debug("Command executed:", " command", command, " arguments", args, " output", output)
+	o.log.Debug("Command executed:", " command", command, " arguments", args, "env vars", cmd.Env, "output", output)
 	return output, err
 }
 
