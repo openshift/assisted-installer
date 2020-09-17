13986a90a71e02037ceca5b2d9d95cb0fecd908f
Moti Asayag <masayag@redhat.com>
2020-09-18 00:50:03 +0300

    OCPBUGSM-17295 Implement Proxy function
    ProxyFromEnvVars provides an alternative to http.ProxyFromEnvironment
    since it is being initialized only once and that happens by k8s before
    proxy settings was obtained from the cluster.
    While this is no issue for k8s client, it prevents any out-of-cluster
    traffic from using the proxy.
    
    By providing implementation of that method, we allow to use env-vars
    set for the pod based on the cluster-wide-proxy.
    
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- src/main/assisted-installer-controller/assisted_installer_main.go
+++ src/main/assisted-installer-controller/assisted_installer_main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"net/url"
+	"os"
 	"sync"
 	"time"
 
 	"github.com/openshift/assisted-installer/src/k8s_client"
+	"golang.org/x/net/http/httpproxy"
 
 	"github.com/kelseyhightower/envconfig"
 	assistedinstallercontroller "github.com/openshift/assisted-installer/src/assisted_installer_controller"
@@ -17,6 +21,11 @@ import (
 var Options struct {
 	ControllerConfig assistedinstallercontroller.ControllerConfig
 }
+
+var (
+	envProxyOnce          sync.Once
+	envVarsProxyFuncValue func(*url.URL) (*url.URL, error)
+)
 
 func main() {
 	logger := logrus.New()
@@ -39,7 +48,9 @@ func main() {
 		log.Fatalf("Failed to set env vars for installer-controller pod %v", err)
 	}
 
-	client, err := inventory_client.CreateInventoryClient(Options.ControllerConfig.ClusterID, Options.ControllerConfig.URL, Options.ControllerConfig.PullSecretToken, Options.ControllerConfig.SkipCertVerification, Options.ControllerConfig.CACertPath, logger)
+	client, err := inventory_client.CreateInventoryClient(Options.ControllerConfig.ClusterID,
+		Options.ControllerConfig.URL, Options.ControllerConfig.PullSecretToken, Options.ControllerConfig.SkipCertVerification,
+		Options.ControllerConfig.CACertPath, logger, ProxyFromEnvVars)
 	if err != nil {
 		log.Fatalf("Failed to create inventory client %v", err)
 	}
@@ -68,4 +79,27 @@ func main() {
 	done <- true
 	logger.Infof("Waiting fo all go routines to finish")
 	wg.Wait()
+}
+
+// ProxyFromEnvVars provides an alternative to http.ProxyFromEnvironment since it is being initialized only
+// once and that happens by k8s before proxy settings was obtained. While this is no issue for k8s, it prevents
+// any out-of-cluster traffic from using the proxy
+func ProxyFromEnvVars(req *http.Request) (*url.URL, error) {
+	return envVarsProxyFunc()(req.URL)
+}
+
+func envVarsProxyFunc() func(*url.URL) (*url.URL, error) {
+	envProxyOnce.Do(func() {
+		envVarsProxyFuncValue = FromEnvVars().ProxyFunc()
+	})
+	return envVarsProxyFuncValue
+}
+
+func FromEnvVars() *httpproxy.Config {
+	return &httpproxy.Config{
+		HTTPProxy:  os.Getenv("HTTP_PROXY"),
+		HTTPSProxy: os.Getenv("HTTPS_PROXY"),
+		NoProxy:    os.Getenv("NO_PROXY"),
+		CGI:        os.Getenv("REQUEST_METHOD") != "",
+	}
 }
