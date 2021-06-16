34d09a8fe7a7f892bf0bc26fac5410913481a0eb
Mateusz Kowalski <mko@redhat.com>
2021-06-17 00:38:00 +0200

    Bug 1951812: Remove label from AI namespace after cluster installation (#301)
    Currently the `assisted-installer` namespace on the cluster installed
    using Assisted Installer is labeled with run-level what causes any
    workload running there to bypass security constrains. In a scenario when
    an operator tries to reuse this namespace to deploy Assisted Service on
    such a cluster, the installation will fail because services expect to be
    run using the safe UID/GID ranges.
    
    In order to overcome the issue, after the installation of the
    cluster we will remove the label from the `assisted-installer` namespace
    so that if an operator wants to deploy Assisted Service, it will be
    necessary to recreate the namespace (this time without setting a run-level).
    
    Please note that if for any reason removing the label fails, we will not
    fail the installation of the cluster.
    
    Long-term solution is to remove run-level label from the initially
    created namespace and use another mechanism to ensure that the AI
    controller starts as early as possible. Those efforts are tracked in bug
    1966621.
    
    Closes: OCPBUGSM-28041

--- src/assisted_installer_controller/assisted_installer_controller_test.go
+++ src/assisted_installer_controller/assisted_installer_controller_test.go
@@ -48,6 +48,8 @@ var (
 		MustGatherImage:       "quay.io/test-must-gather:latest",
 	}
 
+	aiNamespaceRunlevelPatch = []byte(`{"metadata":{"labels":{"$patch": "delete", "openshift.io/run-level":"0"}}}`)
+
 	progressClusterVersionCondition = &configv1.ClusterVersion{
 		Status: configv1.ClusterVersionStatus{
 			Conditions: []configv1.ClusterOperatorStatusCondition{{Type: configv1.OperatorProgressing,
@@ -529,6 +531,9 @@ var _ = Describe("installer HostRoleMaster role", func() {
 					fmt.Errorf("bad-conditions-status")).Times(1)
 				setConsoleAsAvailable("cluster-id")
 
+				// Patching NS
+				mockk8sclient.EXPECT().PatchNamespace(defaultTestControllerConf.Namespace, aiNamespaceRunlevelPatch).Return(nil)
+
 				// CVO
 				mockk8sclient.EXPECT().GetClusterVersion("version").Return(nil, fmt.Errorf("dummy")).Times(1)
 
@@ -591,6 +596,9 @@ var _ = Describe("installer HostRoleMaster role", func() {
 				mockbmclient.EXPECT().CompleteInstallation(gomock.Any(), "cluster-id", true, "").Return(fmt.Errorf("dummy")).Times(1)
 				mockbmclient.EXPECT().CompleteInstallation(gomock.Any(), "cluster-id", true, "").Return(nil).Times(1)
 
+				// Patching NS
+				mockk8sclient.EXPECT().PatchNamespace(defaultTestControllerConf.Namespace, aiNamespaceRunlevelPatch).Return(nil)
+
 				wg.Add(1)
 				assistedController.PostInstallConfigs(context.TODO(), &wg, status)
 				wg.Wait()
@@ -602,6 +610,9 @@ var _ = Describe("installer HostRoleMaster role", func() {
 				mockk8sclient.EXPECT().GetConfigMap(gomock.Any(), gomock.Any()).Return(nil, fmt.Errorf("aaa")).MinTimes(1)
 				mockbmclient.EXPECT().CompleteInstallation(gomock.Any(), "cluster-id", false,
 					"Timeout while waiting router ca data").Return(nil).Times(1)
+
+				// Patching NS
+				mockk8sclient.EXPECT().PatchNamespace(defaultTestControllerConf.Namespace, aiNamespaceRunlevelPatch).Return(nil)
 
 				wg.Add(1)
 				go assistedController.PostInstallConfigs(context.TODO(), &wg, status)
@@ -637,6 +648,9 @@ var _ = Describe("installer HostRoleMaster role", func() {
 				mockbmclient.EXPECT().CompleteInstallation(gomock.Any(), "cluster-id", true, "").Return(fmt.Errorf("dummy")).Times(1)
 				mockbmclient.EXPECT().CompleteInstallation(gomock.Any(), "cluster-id", true, "").Return(nil).Times(1)
 
+				// Patching NS
+				mockk8sclient.EXPECT().PatchNamespace(defaultTestControllerConf.Namespace, aiNamespaceRunlevelPatch).Return(nil)
+
 				wg.Add(1)
 				assistedController.PostInstallConfigs(context.TODO(), &wg, status)
 				wg.Wait()
@@ -656,6 +670,9 @@ var _ = Describe("installer HostRoleMaster role", func() {
 				mockbmclient.EXPECT().UpdateClusterOperator(gomock.Any(), "cluster-id", "lso", models.OperatorStatusProgressing, gomock.Any()).Return(nil).AnyTimes()
 				mockbmclient.EXPECT().UpdateClusterOperator(gomock.Any(), "cluster-id", "lso", models.OperatorStatusFailed, "Waiting for operator timed out").Return(nil).Times(1)
 				mockbmclient.EXPECT().CompleteInstallation(gomock.Any(), "cluster-id", true, "").Return(nil).Times(1)
+
+				// Patching NS
+				mockk8sclient.EXPECT().PatchNamespace(defaultTestControllerConf.Namespace, aiNamespaceRunlevelPatch).Return(nil)
 
 				wg.Add(1)
 				assistedController.PostInstallConfigs(context.TODO(), &wg, status)
