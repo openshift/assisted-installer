1fe6d773f3afcacfc4d3833e16fa7ab6d53460c6
Paul Maidment <pmaidmen@redhat.com>
2024-10-06 10:45:49 +0300

    ACM-14592: Libraries bump to mitigate CVE-2024-27289 (#912)
    The goal of this PR is to ensure that the github.com/jackc/pgx library is no longer on 4.16.0 which is vulnerable to CVE-2024-27289
    
    By replacing the library, we achieve this.

--- vendor/github.com/openshift/assisted-service/restapi/embedded_spec.go
+++ vendor/github.com/openshift/assisted-service/restapi/embedded_spec.go
@@ -6404,6 +6404,10 @@ func init() {
         "org_id": {
           "type": "string"
         },
+        "org_soft_timeouts_enabled": {
+          "description": "Indication if organization soft timeouts is enabled for the cluster.",
+          "type": "boolean"
+        },
         "platform": {
           "$ref": "#/definitions/platform"
         },
@@ -7686,7 +7690,6 @@ func init() {
       "description": "Cluster finalizing stage managed by controller",
       "type": "string",
       "enum": [
-        "Waiting for finalizing",
         "Waiting for cluster operators",
         "Adding router ca",
         "Applying olm manifests",
@@ -7786,6 +7789,10 @@ func init() {
           "format": "uuid",
           "x-go-custom-tag": "gorm:\"foreignkey:Cluster\"",
           "x-nullable": true
+        },
+        "connection_timed_out": {
+          "description": "Indicate that connection to assisted service was timed out when soft timeout is enabled.",
+          "type": "boolean"
         },
         "connectivity": {
           "type": "string",
@@ -8894,6 +8901,10 @@ func init() {
         "must_gather_image": {
           "description": "Must-gather images to use",
           "type": "string"
+        },
+        "notify_num_reboots": {
+          "description": "If true, notify number of reboots by assisted controller",
+          "type": "boolean"
         },
         "openshift_version": {
           "description": "Version of the OpenShift cluster.",
@@ -17044,6 +17055,10 @@ func init() {
         "org_id": {
           "type": "string"
         },
+        "org_soft_timeouts_enabled": {
+          "description": "Indication if organization soft timeouts is enabled for the cluster.",
+          "type": "boolean"
+        },
         "platform": {
           "$ref": "#/definitions/platform"
         },
@@ -18300,7 +18315,6 @@ func init() {
       "description": "Cluster finalizing stage managed by controller",
       "type": "string",
       "enum": [
-        "Waiting for finalizing",
         "Waiting for cluster operators",
         "Adding router ca",
         "Applying olm manifests",
@@ -18400,6 +18414,10 @@ func init() {
           "format": "uuid",
           "x-go-custom-tag": "gorm:\"foreignkey:Cluster\"",
           "x-nullable": true
+        },
+        "connection_timed_out": {
+          "description": "Indicate that connection to assisted service was timed out when soft timeout is enabled.",
+          "type": "boolean"
         },
         "connectivity": {
           "type": "string",
@@ -19510,6 +19528,10 @@ func init() {
         "must_gather_image": {
           "description": "Must-gather images to use",
           "type": "string"
+        },
+        "notify_num_reboots": {
+          "description": "If true, notify number of reboots by assisted controller",
+          "type": "boolean"
         },
         "openshift_version": {
           "description": "Version of the OpenShift cluster.",
